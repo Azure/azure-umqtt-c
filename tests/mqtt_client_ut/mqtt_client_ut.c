d9c78bc3d934008f01c6097b34919ac9ddb1b393
Ville Palo <ville.palo@cybercom.com>
2018-04-26 07:28:13 +0300

    Set xioHandle to null when disconnecting

--- tests/mqtt_client_ut/mqtt_client_ut.c
+++ tests/mqtt_client_ut/mqtt_client_ut.c
@@ -1965,7 +1965,9 @@ TEST_FUNCTION(mqtt_client_dowork_does_nothing_if_disconnected_2)
     (void)mqtt_client_connect(mqttHandle, TEST_IO_HANDLE, &mqttOptions);
     umock_c_reset_all_calls();
 
-    STRICT_EXPECTED_CALL(xio_dowork(IGNORED_PTR_ARG));
+    //
+    // If client is disconnected, mqtt_client_dowork shall do nothing, this is why there are no expected calls here.
+    //
 
     // act
     int result = mqtt_client_disconnect(mqttHandle, NULL, NULL);
