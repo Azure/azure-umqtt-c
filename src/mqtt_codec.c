fab6bf45a5c6d21754f30f21e7cbaae57d3b84a8
Eric Wolz <ericwol@microsoft.com>
2020-12-14 11:32:58 -0800

    fix build break

--- src/mqtt_codec.c
+++ src/mqtt_codec.c
@@ -1114,6 +1114,9 @@ int mqtt_codec_bytesReceived(MQTTCODEC_HANDLE handle, const unsigned char* buffe
                             break;
 
                         case PUBLISH_TYPE:
+                        case CONTROL_PACKET_TYPE_INVALID: 
+                        case PACKET_TYPE_ERROR:
+                        case UNKNOWN_TYPE:
                             break;
                     }
                 }
