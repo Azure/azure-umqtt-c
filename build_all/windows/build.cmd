73c89d7ebbf781d8e0baec1ac2613172c3cb51d4
Eric Wolz <ericwol@microsoft.com>
2021-08-13 14:33:13 -0700

    use 1ES hosted agents

--- build_all/windows/build.cmd
+++ build_all/windows/build.cmd
@@ -101,6 +101,14 @@ echo %build-root%\cmake\%CMAKE_DIR%
 mkdir %build-root%\cmake\%CMAKE_DIR%
 pushd %build-root%\cmake\%CMAKE_DIR%
 
+echo ***checking msbuild***
+where /q msbuild
+IF ERRORLEVEL 1 (
+echo ***setting VC paths***
+    IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
+)
+where msbuild
+
 if %MAKE_NUGET_PKG% == yes (
     echo ***Running CMAKE for Win32***
     cmake %build-root% -Drun_unittests:BOOL=%CMAKE_run_unittests%
