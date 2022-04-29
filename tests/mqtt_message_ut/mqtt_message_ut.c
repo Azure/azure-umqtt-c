16b33278952b91fadd003a43e5658d5569312ce9
Dane Walton <dawalton@microsoft.com>
2022-04-28 18:16:05 -0700

    fix compile

--- tests/mqtt_message_ut/mqtt_message_ut.c
+++ tests/mqtt_message_ut/mqtt_message_ut.c
@@ -79,7 +79,7 @@ static bool g_fail_alloc_calls;
 
 static const uint8_t TEST_PACKET_ID = (uint8_t)0x12;
 static const char* TEST_TOPIC_NAME = "$subTopic1/subTopic2/subTopic3/?$prop1=value1&$prop2=value2";
-static const uint8_t* TEST_MESSAGE = (const uint8_t *)TEST_MESSAGE_TEXT
+static const uint8_t* TEST_MESSAGE = (const uint8_t *)TEST_MESSAGE_TEXT;
 static const int TEST_MSG_LEN = sizeof(TEST_MESSAGE_TEXT) - 1;
 
 typedef struct TEST_COMPLETE_DATA_INSTANCE_TAG
